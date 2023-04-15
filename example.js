97d1443b89f297fb9989afc85fa613ad290991f1
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-04-16 00:18:56 +0900

    first commit

---
+++ example.js
@@ -0,0 +1,69 @@
+
+
+// Synchrounus and Asychronous 
+
+//JS =>  sinxron, blocking, single - threaded dasturlash tili hisoblanadi
+
+
+
+// sinxron 
+
+// function A() {
+//     console.log('A');
+
+// }
+
+// function B() {
+//     console.log('B');
+
+// }
+
+// B()
+// A()
+
+
+
+
+
+//Asynchronous 
+//setTimeout
+
+// console.log('1');
+
+
+
+// setTimeout(() => {
+//     console.log('Hello G20 group');
+// }, 1000)
+
+
+// console.log('2');
+
+
+
+
+
+//setInterval
+
+// setInterval(()=>{
+//     let date = new Date();
+//     console.log(`Current time: ${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`);
+// }, 2000)
+
+
+
+///ERROR HANDLING
+
+
+// const add = (a,b) => {
+//     return a + b
+// }
+
+// try{
+//     console.log(add(10,20));
+// } catch(error){
+//     console.log(error);
+// }
+
+
+
---
+++ task1.js
@@ -0,0 +1,12 @@
+
+
+/// positive to negative, negative to positive number
+
+
+function invert(array) {
+    return array.map(num => num * -1)
+  
+  }
+  console.log(invert([-10,22,-4,9]));
+  
+//   invert([-10,22,-4,9])
